1bbe5cd443a8522c0be8c1b025e9d6354dde6a17
Mikhail Kamorin <m.v.kamorin@gmail.com>
2021-02-12 19:17:09 +0300

    Add Subscription entity and its processing

---
+++ src/Entity/Subscription.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="subscription")
+ * @ORM\Entity
+ */
+class Subscription
+{
+    /**
+     * @ORM\Column(name="id", type="bigint", unique=true)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private int $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="subscriptionAuthors")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="author_id", referencedColumnName="id")
+     * })
+     */
+    private User $author;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="subscriptionFollowers")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="follower_id", referencedColumnName="id")
+     * })
+     */
+    private User $follower;
+
+    /**
+     * @ORM\Column(name="created_at", type="datetime", nullable=false)
+     */
+    private DateTime $createdAt;
+
+    /**
+     * @var DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime", nullable=false)
+     */
+    private DateTime $updatedAt;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getAuthor(): User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(User $author): void
+    {
+        $this->author = $author;
+    }
+
+    public function getFollower(): User
+    {
+        return $this->follower;
+    }
+
+    public function setFollower(User $follower): void
+    {
+        $this->follower = $follower;
+    }
+
+    public function getCreatedAt(): DateTime {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(): void {
+        $this->createdAt = new DateTime();
+    }
+
+    public function getUpdatedAt(): DateTime {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(): void {
+        $this->updatedAt = new DateTime();
+    }
+}

a9ba3d24a3bc6b4b3039c3206a7b86472ad178c7
Mikhail Kamorin <m.v.kamorin@gmail.com>
2021-02-12 19:38:16 +0300

    Add findByCriteria method to UserService

--- src/Controller/WorldController.php
+++ src/Controller/WorldController.php
@@ -18,7 +18,7 @@ class WorldController extends AbstractController
 
     public function hello(): Response
     {
-        $users = $this->userService->findUsersByLogin('Ivan Ivanov');
+        $users = $this->userService->findUsersByCriteria('J.R.R. Tolkien');
 
         return $this->json(array_map(static fn(User $user) => $user->toArray(), $users));
     }

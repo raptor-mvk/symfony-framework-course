40f1be1b8e16ddad3102c18f59c9efa766487cc8
Mikhail Kamorin <m.v.kamorin@gmail.com>
2021-02-12 17:14:30 +0300

    Add user persist & flush to database

--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -3,15 +3,25 @@
 namespace App\Service;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 
 class UserService
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     public function create(string $login): User
     {
         $user = new User();
         $user->setLogin($login);
         $user->setCreatedAt();
         $user->setUpdatedAt();
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
 
         return $user;
     }

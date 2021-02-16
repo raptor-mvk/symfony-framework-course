cfba95c7c0071e6a8deddb696a02f60614625b38
Mikhail Kamorin <m.v.kamorin@gmail.com>
2021-02-16 17:32:37 +0300

    Fix names of indexes & foreign keys in migration

--- migrations/Version20210215194227.php
+++ migrations/Version20210215194227.php
@@ -27,23 +27,23 @@ final class Version20210215194227 extends AbstractMigration
         $this->addSql('CREATE INDEX tweet__author_id__ind ON tweet (author_id)');
         $this->addSql('CREATE TABLE "user" (id BIGSERIAL NOT NULL, login VARCHAR(32) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE author_follower (author_id BIGINT NOT NULL, follower_id BIGINT NOT NULL, PRIMARY KEY(author_id, follower_id))');
-        $this->addSql('CREATE INDEX IDX_564623F3F675F31B ON author_follower (author_id)');
-        $this->addSql('CREATE INDEX IDX_564623F3AC24F853 ON author_follower (follower_id)');
-        $this->addSql('ALTER TABLE subscription ADD CONSTRAINT FK_A3C664D3F675F31B FOREIGN KEY (author_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE subscription ADD CONSTRAINT FK_A3C664D3AC24F853 FOREIGN KEY (follower_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE tweet ADD CONSTRAINT FK_3D660A3BF675F31B FOREIGN KEY (author_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE author_follower ADD CONSTRAINT FK_564623F3F675F31B FOREIGN KEY (author_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE author_follower ADD CONSTRAINT FK_564623F3AC24F853 FOREIGN KEY (follower_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE INDEX author_follower__author_id__ind ON author_follower (author_id)');
+        $this->addSql('CREATE INDEX author_follower__follower_id__ind ON author_follower (follower_id)');
+        $this->addSql('ALTER TABLE subscription ADD CONSTRAINT subscription__author_id__fk FOREIGN KEY (author_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE subscription ADD CONSTRAINT subscription__follower_id__fk FOREIGN KEY (follower_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE tweet ADD CONSTRAINT tweet__author_id__fk FOREIGN KEY (author_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE author_follower ADD CONSTRAINT author_follower__author_id__fk FOREIGN KEY (author_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE author_follower ADD CONSTRAINT author_follower__follower_id__fk FOREIGN KEY (follower_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE subscription DROP CONSTRAINT FK_A3C664D3F675F31B');
-        $this->addSql('ALTER TABLE subscription DROP CONSTRAINT FK_A3C664D3AC24F853');
-        $this->addSql('ALTER TABLE tweet DROP CONSTRAINT FK_3D660A3BF675F31B');
-        $this->addSql('ALTER TABLE author_follower DROP CONSTRAINT FK_564623F3F675F31B');
-        $this->addSql('ALTER TABLE author_follower DROP CONSTRAINT FK_564623F3AC24F853');
+        $this->addSql('ALTER TABLE subscription DROP CONSTRAINT author_follower__follower_id__fk');
+        $this->addSql('ALTER TABLE subscription DROP CONSTRAINT author_follower__author_id__fk');
+        $this->addSql('ALTER TABLE tweet DROP CONSTRAINT tweet__author_id__fk');
+        $this->addSql('ALTER TABLE author_follower DROP CONSTRAINT subscription__follower_id__fk');
+        $this->addSql('ALTER TABLE author_follower DROP CONSTRAINT subscription__author_id__fk');
         $this->addSql('DROP TABLE subscription');
         $this->addSql('DROP TABLE tweet');
         $this->addSql('DROP TABLE "user"');

c26d788e6d4a9ab6e7f9392d0c9e68a61fc1e459
Mikhail Kamorin <m.v.kamorin@gmail.com>
2021-03-10 20:16:53 +0300

    Add redirect after successful login

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -50,9 +50,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse('/api/v1/user');
     }
 
     protected function getLoginUrl(Request $request): string

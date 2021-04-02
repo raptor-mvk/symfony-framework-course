3c909a625f94fe451f9c80999c986a120e7c906a
Mikhail Kamorin <m.v.kamorin@gmail.com>
2021-04-02 18:59:17 +0300

    Combine search and aggregation

--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -8,6 +8,7 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Elastica\Aggregation\Terms;
 use Elastica\Query;
+use Elastica\Query\QueryString;
 use FOS\ElasticaBundle\Finder\PaginatedFinderInterface;
 use FOS\ElasticaBundle\Paginator\FantaPaginatorAdapter;
 use JsonException;
@@ -155,6 +156,22 @@ class UserService
         $aggregation = new Terms('notifications');
         $aggregation->setField($field);
         $query = new Query();
+        $query->addAggregation($aggregation);
+        $paginatedResult = $this->finder->findPaginated($query);
+        /** @var FantaPaginatorAdapter $adapter */
+        $adapter = $paginatedResult->getAdapter();
+
+        return $adapter->getAggregations();
+    }
+
+    /**
+     * @return User[]
+     */
+    public function findUserByQueryWithAggregation(string $queryString, string $field): array
+    {
+        $aggregation = new Terms('notifications');
+        $aggregation->setField($field);
+        $query = new Query(new QueryString($queryString));
         $query->addAggregation($aggregation);
         $paginatedResult = $this->finder->findPaginated($query);
         /** @var FantaPaginatorAdapter $adapter */

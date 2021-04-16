1b5700e558ce3b68d6845a01a634f449ede46d84
Mikhail Kamorin <m.v.kamorin@gmail.com>
2021-04-16 18:54:18 +0300

    Add progress bar

--- src/Command/AddFollowersCommand.php
+++ src/Command/AddFollowersCommand.php
@@ -2,9 +2,13 @@
 
 namespace App\Command;
 
+use App\DTO\UserDTO;
+use App\Entity\User;
 use App\Service\SubscriptionService;
 use App\Service\UserService;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Command\LockableTrait;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -12,6 +16,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 final class AddFollowersCommand extends Command
 {
+    use LockableTrait;
+
     /** @var int */
     public const OK = 0;
     /** @var int */
@@ -45,6 +51,10 @@ final class AddFollowersCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        if (!$this->lock()) {
+            $output->writeln('<info>Command is already running.</info>');
+            return self::OK;
+        }
         $authorId = (int)$input->getArgument('authorId');
         $user = $this->userService->findUserById($authorId);
         if ($user === null) {
@@ -56,9 +66,30 @@ final class AddFollowersCommand extends Command
             $output->write("<error>Count should be positive integer</error>\n");
             return self::GENERAL_ERROR;
         }
-        $login = $input->getOption('login') ?? self::DEFAULT_LOGIN_PREFIX;
-        $result = $this->subscriptionService->addFollowers($user, $login.$authorId, $count);
-        $output->write("<info>$result followers were created</info>\n");
+        $loginPrefix = $input->getOption('login') ?? self::DEFAULT_LOGIN_PREFIX;
+        $progressBar = new ProgressBar($output, $count);
+        $progressBar->start();
+        $createdFollowers = 0;
+        for ($i = 0; $i < $count; $i++) {
+            $login = $loginPrefix.$authorId."_#$i";
+            $password = $login;
+            $age = $i;
+            $isActive = true;
+            $phone = '+'.str_pad((string)abs(crc32($login)), 10, '0');
+            $email = "$login@gmail.com";
+            $preferred = random_int(0, 1) === 1 ? User::EMAIL_NOTIFICATION : User::SMS_NOTIFICATION;
+            $data = compact('login', 'password', 'age', 'isActive', 'phone', 'email', 'preferred');
+            $followerId = $this->userService->saveUser(new User(), new UserDTO($data));
+            if ($followerId !== null) {
+                $this->subscriptionService->subscribe($user->getId(), $followerId);
+                $createdFollowers++;
+                usleep(100000);
+                $progressBar->advance();
+            }
+        }
+        $output->write("<info>$createdFollowers followers were created</info>\n");
+        $progressBar->finish();
+
         return self::OK;
     }
 }
